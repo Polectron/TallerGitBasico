aa0344f5c6af8dd2a05ddf6416800144f92c7d1c
polectron <polectron@gmail.com>
2015-11-23 17:45:57 +0100

    Corregir error en linea 19

--- esPrimo.py
+++ esPrimo.py
@@ -1,32 +1,32 @@
 """
-Autor: Daniel Rückert García
+Autor: Daniel Ruckert Garcia
 """
 
 
 #Nota: los primos son: Naturales, > 1, divisibles entre ellos mismo y entre 1
 def esPrimo(n):
-	"""
-	Metodo para comprobar si un numero n dado es primo o no
-	params: un numero n
-	return: True si es primo y False de no serlo
-	"""
-	
-	#Si es menor que 2 no puede ser primo
-	if n < 2:	return False
-	
-	#El rango de 2 hasta n
-	for i in range(2, n):
-		if n / i == 1:	return False
-	
-	return True
+    """
+    Metodo para comprobar si un numero n dado es primo o no
+    params: un numero n
+    return: True si es primo y False de no serlo
+    """
+
+    #Si es menor que 2 no puede ser primo
+    if n < 2:    return False
+
+    #El rango de 2 hasta n
+    for i in range(2, n):
+        if n % i == 0:    return False
+
+    return True
 
 def esPrimoIntervalo(i, k):
-	"""
-	Metodo que comprueba si los numeros de un intervalo son primos o no
-	params: intervalo [i ,k)
-	return: retorna una lista de duplas
-	"""
-	return [(x, esPrimo(x)) for x in range(i, k)]
+    """
+    Metodo que comprueba si los numeros de un intervalo son primos o no
+    params: intervalo [i ,k)
+    return: retorna una lista de duplas
+    """
+    return [(x, esPrimo(x)) for x in range(i, k)]
 
 
 """
@@ -38,4 +38,9 @@ print [(x, esPrimo(x)) for x in primos]
 """
 #Probando que funciona <<esPrimoIntervalo(n)>>
 print esPrimoIntervalo(0, 1000)
+"""
+
+"""
+primos = [3, 5, 7, 8, 11, 13, 17, 19, 23, 29]
+print [(x, esPrimo(x)) for x in primos]
 """
